1ba318a557c7b5c3fc0bff62159a7d37a993fd2e
Siddhant Mehta <2010015323@adit.ac.in>
2021-08-01 11:54:44 +0530

    ui made

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,16 +1,54 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
+
   @override
-  _HomePageState createState() => _HomePageState();
+  _homepageState createState() => _homepageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    
-    throw UnimplementedError();
-
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Speech to Text"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              child: Card(
+                color: Colors.black,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ListTile(
+                      title: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.mic),
+                        color: Colors.red,
+                        iconSize: 75,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+                height: 250,
+                child: Card(
+                  color: Colors.grey,
+                ))
+          ],
+        ),
+      ),
+      backgroundColor: Colors.teal,
+    );
   }
 }
